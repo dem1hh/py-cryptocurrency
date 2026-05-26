6893d3cd11b776ac82ba57e790b155db9cb4fe6b
Dima Sukhariev <dmitriu.sy1303@gmail.com>
2026-05-26 15:46:49 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,30 @@
-# write your code here
+from unittest import mock
+from unittest.mock import MagicMock
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_get_exchange_rate_prediction_to_buy(
+        mock_exchange: MagicMock
+) -> None:
+    mock_exchange.return_value = 120
+
+    assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_get_exchange_rate_prediction_to_sell(
+        mock_exchange: MagicMock
+) -> None:
+    mock_exchange.return_value = 80
+
+    assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_get_exchange_rate_prediction_to_do_nothing(
+        mock_exchange: MagicMock
+) -> None:
+    mock_exchange.return_value = 101
+
+    assert cryptocurrency_action(100) == "Do nothing"
